3b44a3f8353f9c3ef07cadc5b41dfba2b21c79f1
David Chauviere <d_chauviere@yahoo.fr>
2021-09-14 09:22:48 +0200

    repair go destination

--- modules/go/setup.sh
+++ modules/go/setup.sh
@@ -2,5 +2,5 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/../../lib/wildenv.lib.sh"
 
-mkdir -p "${WILDENV_ROOT_DIR}/share"
-curl -sL "https://golang.org/dl/go1.17.linux-amd64.tar.gz" | tar xzf - --strip-components=1 -C "${WILDENV_ROOT_DIR}/share"
+mkdir -p "${WILDENV_ROOT_DIR}/share/go"
+curl -sL "https://golang.org/dl/go1.17.linux-amd64.tar.gz" | tar xzf - --strip-components=1 -C "${WILDENV_ROOT_DIR}/share/go"
