ec06c8ac36fcf8be5ff2b5c563dd7223dd1398a7
David Chauvière <david@chauviere.net>
2021-09-22 13:47:53 +0200

    add k3d for kubernetes

--- modules/kubernetes/setup.sh
+++ modules/kubernetes/setup.sh
@@ -9,3 +9,6 @@ curl -sL -o "${WILDENV_ROOT_DIR}/etc/bash/completion/kubectx" "https://github.co
 curl -sL -o "${WILDENV_ROOT_DIR}/etc/bash/completion/kubens" "https://github.com/ahmetb/kubectx/raw/master/completion/kubens.bash"
 
 curl -sL "https://get.helm.sh/helm-v3.6.3-linux-amd64.tar.gz" |tar xz -C "${WILDENV_ROOT_DIR}/bin/" --strip-components=1 linux-amd64/helm
+
+curl -sL -o "${WILDENV_ROOT_DIR}/bin/k3d" "https://github.com/rancher/k3d/releases/download/v4.4.8/k3d-linux-amd64"
+chmod a+x "${WILDENV_ROOT_DIR}/bin/k3d"
