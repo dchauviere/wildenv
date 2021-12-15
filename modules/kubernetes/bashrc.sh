b2a7648289abd0ba50ff0c9c087d34baa23e5075
David Chauviere <d_chauviere@yahoo.fr>
2021-12-15 16:25:57 +0100

    kubectl tooling

--- modules/kubernetes/bashrc.sh
+++ modules/kubernetes/bashrc.sh
@@ -3,20 +3,47 @@
 source <(kubectl completion bash)
 source "${WILDENV_ROOT_DIR}/etc/bash/completion/kubectx"
 source "${WILDENV_ROOT_DIR}/etc/bash/completion/kubens"
-alias k=kubectl
-complete -F __start_kubectl k
 
-function _kubectl_helper() {
-  local _cmd=$@
-  local _object=$(kubectl get all --no-headers -o name | fzf --info=inline --layout=reverse --border --prompt "$(kubectl config current-context | sed 's/-context$//')> " --preview-window "right,follow" --preview "kubectl describe {}")
-  [[ "$_object" = "" ]]&& return
-  kubectl $_cmd "${_object%% *}"
+function _khelper() {
+  local _cmd=$1
+  shift
+  local _args=("$@")
+
+  case "$_cmd" in
+  describe)
+    kubectl get all,networkpolicies,sa,roles,rolebindings,configmaps,secrets "${_args[@]}" --no-headers -o name | \
+      fzf --info=inline \
+          --layout=reverse \
+          --border \
+          --prompt "Describe> " \
+          --preview-window "right" \
+          --preview "kubectl describe {} ${_args[*]}| bat -l yaml --color always --style=plain"
+    ;;
+  logs)
+    _object=$(kubectl get pods,daemonsets,deployments,replicasets "${_args[@]}" --no-headers -o name | \
+      fzf --info=inline \
+          --layout=reverse \
+          --border \
+          --prompt "Logs> " \
+          --preview-window "bottom:75%,follow" \
+          --preview "kubectl logs ${_args[*]} -f --tail=200 {}"
+    )
+    [[ "$_object" = "" ]]&& return
+    kubectl logs -f --tail=200 "${_args[@]}" "${_object%% *}"
+    ;;
+  esac
 }
 
-alias kl="_kubectl_helper logs"
-alias kd="_kubectl_helper describe"
-alias kx="kubectl get pods --no-headers | fzf | awk '{print $1}' | xargs -o -I % kubectl exec -it % bash"
+alias k=kubectl
+alias kl="_khelper logs"
+alias kd="_khelper describe"
+alias kx="kubectl exec -ti"
+alias kn="kubens"
+alias kc="kubectx"
+alias kg="kubectl get all,sa,networkpolicies,roles,rolebindings,configmaps,secrets"
 
-_fzf_setup_completion path kubectl k
+complete -F __start_kubectl kx
+complete -F __start_kubectl k
+complete -F __start_kubectl kg
 
 eval "$(k3d completion bash)"
